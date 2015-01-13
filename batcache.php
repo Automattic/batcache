f174c98bb4dd76c22524204757685b993896186a
salvoaranzulla <salvatore.aranzulla@gmail.com>
2015-01-13 08:27:14 +0100

    New Batcache Manager

--- batcache.php
+++ batcache.php
@@ -8,38 +8,44 @@ Author URI: http://andyskelton.com/
 Version: 1.2
 */
 
-// Do not load if our advanced-cache.php isn't loaded
 if ( ! isset( $batcache ) || ! is_object($batcache) || ! method_exists( $wp_object_cache, 'incr' ) )
 	return;
 
-$batcache->configure_groups();
+function batcache_post_unpublished($new_status, $old_status, $post) {
+    if ($old_status == 'publish'  &&  $new_status != 'publish' && get_post_type($post) == 'post') {
 
-// Regen home and permalink on posts and pages
-add_action('clean_post_cache', 'batcache_post');
+		batcache_clear_url(get_permalink($post->ID));
+		batcache_clear_url( get_option('home') );
+		batcache_clear_url( trailingslashit( get_option('home') ) );
+			
+    }
+}
+add_action('transition_post_status', 'batcache_post_unpublished', 10, 3);
 
-// Regen permalink on comments (TODO)
-//add_action('comment_post',          'batcache_comment');
-//add_action('wp_set_comment_status', 'batcache_comment');
-//add_action('edit_comment',          'batcache_comment');
-
-function batcache_post($post_id) {
-	global $batcache;
-
+function batcache_post_published($post_id) {
+	
 	$post = get_post($post_id);
-	if ( empty( $post ) || $post->post_type == 'revision' || get_post_status($post_id) != 'publish' )
+	if (empty($post) || get_post_type($post) != 'post' || get_post_status($post_id) != "publish")
 		return;
 
+	batcache_clear_url(get_permalink($post->ID));
 	batcache_clear_url( get_option('home') );
 	batcache_clear_url( trailingslashit( get_option('home') ) );
-	batcache_clear_url( get_permalink($post_id) );
+	
+	
 }
+add_action('clean_post_cache', 'batcache_post_published');
 
 function batcache_clear_url($url) {
+	
+	if (empty($url))
+		return;
+	
 	global $batcache;
-	if ( empty($url) )
-		return false;
+	
 	$url_key = md5($url);
 	wp_cache_add("{$url_key}_version", 0, $batcache->group);
 	return wp_cache_incr("{$url_key}_version", 1, $batcache->group);
+
 }
 
