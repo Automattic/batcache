3898f0d342f631cba7aee9226174ab1b10bd4270
Andrija Vucinic <andrija.vucinic@gmail.com>
2020-10-28 17:28:00 +0100

    reuse vars

--- advanced-cache.php
+++ advanced-cache.php
@@ -455,7 +455,7 @@ $batcache->generate_keys();
 
 // Get the batcache
 $batcache->cache = wp_cache_get($batcache->key, $batcache->group);
-$is_cached = ! is_array( $batcache->cache );
+$is_cached = is_array( $batcache->cache );
 $has_expired = $is_cached && time() > $batcache->cache['time'] + $batcache->cache['max_age'];
 
 if ( isset( $batcache->cache['version'] ) && $batcache->cache['version'] != $batcache->url_version ) {
@@ -466,7 +466,7 @@ if ( isset( $batcache->cache['version'] ) && $batcache->cache['version'] != $bat
 	$batcache->do = ! $is_cached || $has_expired;
 } else {
 	// No batcache item found, or ready to sample traffic again at the end of the batcache life?
-	if ( !is_array($batcache->cache) || time() >= $batcache->cache['time'] + $batcache->max_age - $batcache->seconds ) {
+	if ( ! $is_cached || time() >= $batcache->cache['time'] + $batcache->max_age - $batcache->seconds ) {
 		wp_cache_add($batcache->req_key, 0, $batcache->group);
 		$batcache->requests = wp_cache_incr($batcache->req_key, 1, $batcache->group);
 
